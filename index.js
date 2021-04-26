75e9ef8edc66d05002d87d8d7bd22601a1052cc3
prince <mrepte700@gmail.com>
2021-04-26 17:13:13 +0600

    completed the api calling and showing profile info

--- index.js
+++ index.js
@@ -7,15 +7,65 @@ async function getUser(name){
     const profile = await res.json();
 
     return profile;
-
 }
 
 document.querySelector('#search').addEventListener('submit', async(e) => {
     e.preventDefault();
     const username = document.querySelector('#findByUsername').value;
-
-    console.log(username);
-
+    
     const profile = await getUser(username);
-    console.log(profile);
+
+    showProfile(profile);
 })
+
+function showProfile(profile){
+    document.querySelector('.profile').innerHTML = `
+
+        <img
+            src="${profile.avatar_url}"
+            alt="${profile.name}"/>
+        </img>    
+        <p class="name">${profile.name}</p>
+        <p class="username login">${profile.login}</p>
+        <p class="bio">
+            ${profile.bio}
+        </p>
+
+        <div class="followers-stars">
+            <p>
+                <ion-icon name="people-outline"></ion-icon>
+                <span class="followers"> ${profile.followers} </span> followers
+            </p>
+                <span class="dot">·</span>
+            <p>
+            <span class="following"> ${profile.following} </span> 
+                following
+            </p>
+        </div>
+
+        <p class="company">
+            <ion-icon name="business-outline"></ion-icon>
+            ${profile.company}
+        </p>
+        <p class="location">
+            <ion-icon name="location-outline"></ion-icon>${profile.location}
+        </p>
+
+    `;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
